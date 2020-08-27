808ff76e692256d52490d334d925a30b71de8ca5
Rahul-singh98 <rahulrajput98fun@gmail.com>
2020-08-27 11:13:34 +0530

    v3 Rolling Dice

--- Rolling Dice/main.py
+++ Rolling Dice/main.py
@@ -1,10 +1,23 @@
 from tkinter import *
 from PIL import ImageTk , Image
 import random 
+# import graphics
 
 root = Tk()
-root.geometry('200x200')
+root.geometry('1200x700')
 root.title('Rolling Dice')
+
+ct = [random.randrange(256) for x in range(3)]
+brightness = int(round(0.299*ct[0] + 0.587*ct[1] + 0.114*ct[2] ))
+root.configure(bg = 'white' if brightness < 120 else 'Black' )
+
+# c = Canvas(root, height = 1200 , width= 700)
+# c.place(x=0 , y= 0)
+# c.configure(bg = "#000000")
+
+# g = graphics.shape('./Dice_Images/dice_obj/dice.obj')
+# g.origin=[0,3,0]
+# g.render()
 
 dice = ['./Dice_Images/Dice1.png','./Dice_Images/Dice2.png',
         './Dice_Images/Dice3.png','./Dice_Images/Dice4.png',
